500c4d7c4292ccc2240bf437e80244ccf6e5788d
Marcin Maka <marcin.maka@linux.intel.com>
2019-01-10 17:33:22 +0100

    import org.sofproject.ui plug-in
    Signed-off-by: Marcin Maka <marcin.maka@linux.intel.com>

---
+++ org.sofproject.ui/src/org/sofproject/ui/views/SofNodeConnectionsViewPart.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright (c) 2018, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.sofproject.ui.views;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Observable;
+import java.util.Observer;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.console.MessageConsoleStream;
+import org.eclipse.ui.part.ViewPart;
+import org.sofproject.core.connection.SofNodeConnection;
+import org.sofproject.core.connection.SofNodeConnectionManager;
+import org.sofproject.core.connection.SofRemoteOperation;
+import org.sofproject.ui.console.SofConsole;
+import org.sofproject.ui.handlers.SofNodeLoginDialog;
+
+public class SofNodeConnectionsViewPart extends ViewPart {
+
+	private TableViewer viewer;
+
+	// TODO: should re-pack the layout on change?
+
+	// TODO: connection is duplicated for the first time a first project is created?
+
+	@Override
+	public void createPartControl(Composite parent) {
+		viewer = new TableViewer(parent,
+				SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
+
+		TableViewerColumn colProjName = new TableViewerColumn(viewer, SWT.NONE);
+		colProjName.getColumn().setWidth(200);
+		colProjName.getColumn().setText("Project");
+		colProjName.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				SofNodeConnection conn = (SofNodeConnection) element;
+				return conn.getProject().getProject().getName();
+			}
+		});
+
+		TableViewerColumn colNodeAddr = new TableViewerColumn(viewer, SWT.NONE);
+		colNodeAddr.getColumn().setWidth(200);
+		colNodeAddr.getColumn().setText("Address");
+		colNodeAddr.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				SofNodeConnection conn = (SofNodeConnection) element;
+				return conn.getNodeDescriptor().getAddr();
+			}
+		});
+
+		TableViewerColumn colResPath = new TableViewerColumn(viewer, SWT.NONE);
+		colResPath.getColumn().setWidth(200);
+		colResPath.getColumn().setText("Resource Path");
+		colResPath.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				SofNodeConnection conn = (SofNodeConnection) element;
+				return conn.getProject().getNodeDescriptor().getResPath();
+			}
+		});
+
+		TableViewerColumn colConnected = new TableViewerColumn(viewer, SWT.NONE);
+		colConnected.getColumn().setWidth(100);
+		colConnected.getColumn().setText("Connected");
+		colConnected.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				SofNodeConnection conn = (SofNodeConnection) element;
+				return conn.isConnected() ? "yes" : "no";
+			}
+		});
+
+		viewer.addSelectionChangedListener(new ISelectionChangedListener() {
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+//				IStructuredSelection selection = viewer.getStructuredSelection();
+//				Object sel = selection.getFirstElement();
+				// TODO: update state of toolbar items
+			}
+		});
+		viewer.setContentProvider(new ArrayContentProvider());
+
+		viewer.getTable().setHeaderVisible(true);
+		viewer.getTable().setLinesVisible(true);
+
+		IActionBars actionBars = getViewSite().getActionBars();
+//		IMenuManager menuManager = actionBars.getMenuManager();
+		IToolBarManager toolbarManager = actionBars.getToolBarManager();
+
+		toolbarManager.add(new Action("Import Files") {
+			@Override
+			public void run() {
+				runOp(SofNodeConnection.createImportResourcesOp(), null);
+			}
+		});
+
+		toolbarManager.add(new Action("Open dmesg") {
+			@Override
+			public void run() {
+				runOp(SofNodeConnection.createConnectDmesgOp(), "dmesg");
+			}
+		});
+
+		toolbarManager.add(new Action("Open logger") {
+			@Override
+			public void run() {
+				runOp(SofNodeConnection.createConnectLoggerOp(), "logger");
+			}
+		});
+
+		SofNodeConnectionManager connMgr = SofNodeConnectionManager.getInstance();
+		viewer.setInput(connMgr.getConnections());
+		connMgr.addObserver(new Observer() {
+			@Override
+			public void update(Observable o, Object arg) {
+				Display.getDefault().asyncExec(new Runnable() {
+					@Override
+					public void run() {
+						viewer.refresh();
+					}
+				});
+			}
+		});
+	}
+
+	private void runOp(SofRemoteOperation op, String consNameExt) {
+		Object el = viewer.getStructuredSelection().getFirstElement();
+		if (el != null) {
+			SofNodeConnection conn = (SofNodeConnection) el;
+			MessageConsoleStream mcs = null;
+			if (consNameExt != null) {
+				mcs = SofConsole.getConsoleStream(
+						conn.getProject().getProject().getName() + "." + consNameExt);
+			}
+			try {
+				connectNode(conn);
+				op.setConnection(conn);
+				op.setOutputStream(mcs);
+
+				new ProgressMonitorDialog(null).run(true, false, new IRunnableWithProgress() {
+
+					@Override
+					public void run(IProgressMonitor monitor)
+							throws InvocationTargetException, InterruptedException {
+						op.run(monitor);
+					}
+				});
+			} catch (CoreException | InvocationTargetException e) {
+				MessageDialog.openError(null, "Operation failed: ", e.getMessage());
+			} catch (InterruptedException e) {
+				MessageDialog.openInformation(null, "Operation canceled", "Operation canceled");
+			}
+			viewer.refresh();
+		}
+	}
+
+	private void connectNode(SofNodeConnection conn) throws CoreException {
+		if (conn.isConnected())
+			return;
+
+		SofNodeLoginDialog dlg = new SofNodeLoginDialog(null,
+				conn.getProject().getProject().getName(), conn.getNodeDescriptor().getAddr());
+		if (dlg.open() == Window.OK) {
+			conn.connect(dlg.getLogin(), dlg.getPass());
+		}
+	}
+
+	@Override
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+
+}
---
+++ org.sofproject.ui/src/org/sofproject/ui/wizards/NewDutWizard.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2018, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.sofproject.ui.wizards;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkingSet;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.sofproject.core.SofNodeProject;
+
+public class NewDutWizard extends Wizard implements INewWizard {
+	private NewConnectionCreationPage connPage;
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		super.addPages();
+		connPage = new NewConnectionCreationPage("NewConnection");
+		connPage.setTitle("New Connection");
+		connPage.setDescription("Create new connection to SOF node");
+		connPage.setImageDescriptor(
+				ImageDescriptor.createFromFile(getClass(), "/icons/sof-logo.png"));
+		addPage(connPage);
+		connPage.init(selection);
+	}
+
+	@Override
+	public boolean performFinish() {
+		try {
+			WorkspaceModifyOperation op = new WorkspaceModifyOperation() {
+
+				@Override
+				protected void execute(IProgressMonitor monitor)
+						throws CoreException, InvocationTargetException, InterruptedException {
+					createConnection(monitor != null ? monitor : new NullProgressMonitor());
+				}
+			};
+			getContainer().run(false, true, op);
+		} catch (InvocationTargetException x) {
+			return false;
+		} catch (InterruptedException x) {
+			return false;
+		}
+		return true;
+	}
+
+	protected void createConnection(IProgressMonitor monitor) throws CoreException {
+		IProject project = SofNodeProject.create(connPage.getProjectName(),
+				connPage.getLocationPath(), connPage.getAddress(), connPage.getRemoteResPath(),
+				monitor);
+		IWorkingSet[] workingSets = connPage.getSelectedWorkingSets();
+		PlatformUI.getWorkbench().getWorkingSetManager().addToWorkingSets(project, workingSets);
+	}
+}
