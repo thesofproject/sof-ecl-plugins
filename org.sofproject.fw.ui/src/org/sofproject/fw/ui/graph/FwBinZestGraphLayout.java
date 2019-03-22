d5f6451fb325dfeb01492d96d1aef615962c693a
Marcin Maka <marcin.maka@linux.intel.com>
2019-03-22 11:15:51 +0100

    ui: separate common base x-y layout for graphs
    Signed-off-by: Marcin Maka <marcin.maka@linux.intel.com>

--- org.sofproject.fw.ui/src/org/sofproject/fw/ui/graph/FwBinZestGraphLayout.java
+++ org.sofproject.fw.ui/src/org/sofproject/fw/ui/graph/FwBinZestGraphLayout.java
@@ -26,104 +26,30 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  */
-
 package org.sofproject.fw.ui.graph;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.gef.geometry.planar.Dimension;
-import org.eclipse.gef.geometry.planar.Point;
-import org.eclipse.gef.graph.Graph;
 import org.eclipse.gef.graph.Node;
-import org.eclipse.gef.layout.ILayoutAlgorithm;
 import org.eclipse.gef.layout.LayoutContext;
-import org.eclipse.gef.layout.LayoutProperties;
-import org.sofproject.fw.model.FwBinGraph;
+import org.sofproject.ui.graph.SofXyZestGraphLayout;
 
-public class FwBinZestGraphLayout implements ILayoutAlgorithm {
+public class FwBinZestGraphLayout extends SofXyZestGraphLayout {
 
-	private static double TOP_MARGIN = 10d;
-	private static double LEFT_MARGIN = 10d;
+	// exclude the first row (fw memory map) from column size computations
+	private static final int TOP_ROWS_EXCLUDED = 1;
 
-	private static double COLUMN_MARGIN = 30d; // need more space for nice arrows
-	private static double ROW_MARGIN = 10;
-
-	List<Double> colSizes = new ArrayList<>();
-	List<Double> rowSizes = new ArrayList<>();
-
-	private void updateColumnSize(int col, double size) {
-		while (colSizes.size() <= col) {
-			colSizes.add(0d);
-		}
-		colSizes.set(col, Double.max(colSizes.get(col), size));
-	}
-
-	private void updateRowSize(int row, double size) {
-		while (rowSizes.size() <= row) {
-			rowSizes.add(0d);
-		}
-		rowSizes.set(row, Double.max(rowSizes.get(row), size));
-	}
-
-	private void computeSizes(Graph graph) {
-		colSizes.clear();
-		rowSizes.clear();
-		for (Node n : graph.getNodes()) {
-			int col = FwBinZestGraphBuilder.getNodePosX(n);
-			int row = FwBinZestGraphBuilder.getNodePosY(n);
-			Dimension size = LayoutProperties.getSize(n);
-
-//			System.out.println("computeSizes ["+col+", "+row+"] "
-//					+ size.getWidth() + " x " + size.getHeight());
-
-			// item in the first row may span across multiple columns, so do not
-			// update the column size common for other rows
-			if (row != FwBinGraph.FW_MEM_MAP_ROW) {
-				updateColumnSize(col, size.getWidth());
-			}
-			updateRowSize(row, size.getHeight());
-		}
-		// now compute accumulated offsets for each column and row
-		double accSize = LEFT_MARGIN;
-		for (int i = 0; i < colSizes.size(); i++) {
-			double colSize = colSizes.set(i, accSize);
-
-//			System.out.println("colSize [" + i + "] = " + accSize);
-
-			accSize += colSize + COLUMN_MARGIN;
-		}
-
-		accSize = TOP_MARGIN;
-		for (int i = 0; i < rowSizes.size(); i++) {
-			double rowSize = rowSizes.set(i, accSize);
-			accSize += rowSize + ROW_MARGIN;
-		}
+	public FwBinZestGraphLayout() {
+		super(TOP_ROWS_EXCLUDED);
 	}
 
 	public void applyLayout(LayoutContext context, boolean clean) {
-
-		// Some nodes are repositioned since the default GraphLayoutBehavior running
-		// in post-layout step attempts to center the nodes at the provided location
-		// when calling node's setPosition() finally.
-
-		Graph g = context.getGraph();
-		if (g.getNodes().isEmpty())
-			return;
-
-		computeSizes(g);
-
-		// setting up fixed grid locations assigned while building the graph
-
-		// TODO: now the nodes are top-left aligned, should be centered inside cells
-
-		for (Node n : context.getGraph().getNodes()) {
-			int col = FwBinZestGraphBuilder.getNodePosX(n);
-			int row = FwBinZestGraphBuilder.getNodePosY(n);
-			Dimension size = LayoutProperties.getSize(n);
-			double x = colSizes.get(col) + size.getWidth() / 2;
-			double y = rowSizes.get(row) + size.getHeight() / 2;
-			LayoutProperties.setLocation(n, new Point(x, y));
+		if (isGridEmpty()) {
+			for (Node n : context.getGraph().getNodes()) {
+				int col = FwBinZestGraphBuilder.getNodePosX(n);
+				int row = FwBinZestGraphBuilder.getNodePosY(n);
+				addToGrid(n, col, row);
+			}
+			gridComplete();
 		}
+		super.applyLayout(context, clean);
 	}
 }
