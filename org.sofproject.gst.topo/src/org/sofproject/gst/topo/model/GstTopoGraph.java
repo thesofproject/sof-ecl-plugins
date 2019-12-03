fcad5119c3c86106f9c69bbc573a8ecc0914636c
Emilia Dominiak <emilia.dominiak@intel.com>
2019-12-03 22:14:38 +0100

    reverte GstTopoGraph.java

--- org.sofproject.gst.topo/src/org/sofproject/gst/topo/model/GstTopoGraph.java
+++ org.sofproject.gst.topo/src/org/sofproject/gst/topo/model/GstTopoGraph.java
@@ -34,8 +34,11 @@ import java.beans.PropertyChangeSupport;
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -48,6 +51,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.window.Window;
 import org.sofproject.core.binfile.BinFile;
 import org.sofproject.core.ops.IRemoteOpsProvider;
+import org.sofproject.gst.json.JsonProperty;
 import org.sofproject.gst.topo.ops.GstGraphOpsProvider;
 import org.sofproject.gst.topo.plugins.GstElement;
 import org.sofproject.gst.topo.plugins.GstPlugin;
@@ -57,6 +61,9 @@ import org.sofproject.topo.ui.graph.ITopoCollectionNode;
 import org.sofproject.topo.ui.graph.ITopoConnection;
 import org.sofproject.topo.ui.graph.ITopoGraph;
 import org.sofproject.topo.ui.graph.ITopoNode;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class GstTopoGraph implements ITopoGraph {
 
@@ -190,6 +197,21 @@ public class GstTopoGraph implements ITopoGraph {
 		os.close();
 
 		fileInput.setContents(new ByteArrayInputStream(os.toByteArray()), true, false, null);
+	}
+	
+	@Override
+	public void serializeJson(JsonProperty jsonProperty) throws CoreException, IOException {
+		try {
+			File file = new File(jsonProperty.getName()+".json");
+			jsonProperty.setTemplate(getPipelineString());
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			ObjectMapper obj = new ObjectMapper();
+			obj.writeValue(writer, jsonProperty);
+			writer.close();
+			
+		}catch(Exception e) {
+			System.out.println(e.toString());
+		}
 	}
 
 	public String getPipelineString() {
